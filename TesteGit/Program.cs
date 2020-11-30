d9e91d4f8e73124878d32ba98ccf2928050ec276
Renoá Nascimento <renoaalmeida@gmail.com>
2020-11-30 19:14:12 -0300

    Acrescentada a mensagem de boa noite

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,7 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Boa noite!");
         }
     }
 }
