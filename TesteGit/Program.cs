a46e591d0260520a90ee024ef13d7348bbbb48f1
Renoá Nascimento <renoaalmeida@gmail.com>
2020-11-30 19:12:33 -0300

    Projeto Iniciado mostrando a mensagem Olá Mundo!

---
+++ TesteGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
